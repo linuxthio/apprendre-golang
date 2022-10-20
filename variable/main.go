fe61c5fc8abaa23d90c48bbfc7910fbd5df2cf6e
djibril thiongane <djibthiongane@gmail.com>
2022-10-20 19:07:42 +0000

    creation de l'exo1

---
+++ variable/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var prenom string
+
+	var nom string
+	prenom = "Djibril"
+	nom = "Thiongane"
+
+	fmt.Printf("prenom : %s | nom : %s \n", prenom, nom)
+
+}
