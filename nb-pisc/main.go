e9d6630549c8be1cc75896b4cff08e6aa510c228
Djibril Thiongane <djibthiongane@gmail.com>
2022-10-24 01:27:05 +0000

    creation d'une fonction pour entrer les valeur manuelement grace a fmt.Scanf

--- nb-pisc/main.go
+++ nb-pisc/main.go
@@ -10,22 +10,40 @@ func nb(a int, b int, c int) {
 	cc := n - c
 	t := aa + bb + cc
 	r := 400 - t
-	fmt.Printf("\npisc 1: %d \npisc 2: %d \npisc 3 : %d \ntotal : %d \nreste : %d\n", aa, bb, cc, t, r)
+	fmt.Println("\n\nLe nombre de place restant pour la piscine zone-01:")
+	fmt.Printf("\npisc 1: %d \npisc 2: %d \npisc 3 : %d \n\ntotal : %d \n\nreste : %d\n", aa, bb, cc, t, r)
+
+}
+
+func setval(nv string) int {
+	var n int
+
+	fmt.Printf("Entrer la valeur de %s : ", nv)
+	fmt.Scanf("%d", &n)
+
+	return n
 
 }
 
 func main() {
-	nb(0, 67, 87)
 
-	var a int
-	a = 0
-	fmt.Printf("valeur de a :")
-	i, e := fmt.Scanf("%d", &a)
-	if i > 0 {
-		fmt.Println("ok ")
-	}
-	if e == nil {
-		fmt.Printf("%d \n", a)
-	}
+	n1 := setval("n1")
+	n2 := setval("n2")
+	n3 := setval("n3")
+	nb(n1, n2, n3)
+
+	/*
+		var a int
+		a = 0
+		fmt.Printf("valeur de a :")
+
+		i, e := fmt.Scanf("%d", &a)
+		if i > 0 {
+			fmt.Println("ok ")
+		}
+		if e == nil {
+			fmt.Printf("%d \n", a)
+		}
+	*/
 
 }
